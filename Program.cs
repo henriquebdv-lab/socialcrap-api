4b66cb3a1c2f3c3425ccae478dac8f399dc317de
Henrique Bettega Dalla Vecchia <henriquebdv@gmail.com>
2026-03-19 22:56:43 -0300

    Update frontend and CORS

--- Program.cs
+++ Program.cs
@@ -3,8 +3,15 @@ using SocialCrap.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Registro de serviços
+// Registro de servicos
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
@@ -21,6 +28,7 @@ app.UseSwaggerUI(c =>
     c.RoutePrefix = string.Empty; // Serve o Swagger em "/"
 });
 
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
